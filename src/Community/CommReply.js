5e7b6f78fc123f1ae3f112cd9b7e660cb4369e40
swchoi00 <celpic-@naver.com>
2024-04-26 05:11:23 +0900

    feature/sw/0418test

--- src/Community/CommReply.js
+++ src/Community/CommReply.js
@@ -6,36 +6,39 @@ import { useNavigate } from 'react-router-dom';
 const CommReply = ({ isAuth, userInfo, id }) => {
 
   const [reply, setReply] = useState({
-    commId: id,
     content: '',
-    member: userInfo.username
+    member: { username: userInfo.username } // 서버에서 요구하는 형식에 맞추어 객체로 전달
   });
   const navigate = useNavigate();
 
-  const changHandler = (e) => {
-    setReply(e.target.value);
+  const [replyList, setReplyList] = useState([]);
+
+  const changeHandler = (e) => {
+    setReply({ ...reply, content: e.target.value }); // content 필드만 업데이트
   }
 
-  useEffect(() => {
-    axiosInstance.get(`/reply/${id}/list`)
-      .then((response) => {
-        setReply(response.data);
-      }).catch((error) => {
-        console.log(error);
-      })
-  }, [])
+  // useEffect(() => {
+  //   axiosInstance.get(`/commReply/${id}/list`)
+  //     .then((response) => {
+  //       setReply(response.data);
+  //     }).catch((error) => {
+  //       console.log(error);
+  //     })
+  // }, [])
 
   console.log(reply);
+  console.log("id : " + id);
+  console.log("댓글 목록" + reply)
 
   return (
     <div className='CommReply'>
       <div className='postReply'>
-        <div className='replyTittle'>댓글</div>
+        <div className='replyTitle'>댓글</div>
         <div className='writeReply'>
-          <textarea className='replyInput' value={reply.content} onChange={changHandler} />
+          <textarea className='replyInput' value={reply.content} onChange={changeHandler} />
           <button className='replyBtn' onClick={() => {
             if (isAuth === true) {
-              axiosInstance.post(`/reply/${id}`, reply)
+              axiosInstance.post(`/commReply/${id}`, reply)
                 .then((response) => {
                   alert(response.data);
                 })
@@ -43,21 +46,22 @@ const CommReply = ({ isAuth, userInfo, id }) => {
                   console.log(error);
                 })
             } else {
-              alert('로그인 후 작성가능합니다.');
+              alert('로그인 후 작성 가능합니다.');
               navigate('/login');
             }
           }}>등록</button>
         </div>
+        {/* 아래는 예시이므로 필요에 따라 수정하셔야 합니다 */}
         <div className='getReply'>
           <div className='nickName-date'>
-            <img src="/static/media/face.786407e39b657bdecd13bdabee73e67b.svg" />
-            <div className='nickName'>닉네임</div>
-            <div className='date'>|  2024-04-15</div>
+            <img src="/static/media/face.786407e39b657bdecd13bdabee73e67b.svg" alt="face icon" />
+            <div className='nickName'>{userInfo.username}</div>
+            <div className='date'>| 2024-04-15</div>
           </div>
           <div className='Content'>내용</div>
           <div className='aa'>
             {
-              userInfo.username === reply.member ?
+              userInfo.username === reply.member.username ?
                 <>
                   <button>수정</button>
                   <button onClick={() => {
